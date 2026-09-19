8d0a2f8ad0712057efa5ce80a1e8efa058b526a0
Elis Suárez <elispastorst@gmail.com>
2026-09-19 11:37:26 -0400

    Delete functions/[slug].js

--- functions/[slug].js
+++
@@ -1,9 +0,0 @@
-export async function onRequest(context) {
-  const { params, env } = context;
-  const slug = params.slug;
-  const linkLargo = await env.KV.get('link:' + slug);
-  if (linkLargo) {
-    return Response.redirect(linkLargo, 302);
-  }
-  return Response.redirect('https://guia-lara.pages.dev', 302);
-}
